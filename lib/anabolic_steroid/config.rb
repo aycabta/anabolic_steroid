0f98ee7b050ae2f067c6983fcd8b77e80f64e23b
Code Ass <aycabta@gmail.com>
2016-10-06 11:40:04 +0900

    Check config file existence

--- lib/anabolic_steroid/config.rb
+++ lib/anabolic_steroid/config.rb
@@ -5,10 +5,14 @@ module AnabolicSteroid
     attr_accessor :entry_date_xpath, :next_page_xpath
 
     def initialize(path)
-      yaml = YAML.load_file(path)
-      @entry_date_xpath = yaml['xpath']['entry_date']
-      @entry_link_xpath = yaml['xpath']['entry_link']
-      @next_page_link_xpath = yaml['xpath']['next_page_link']
+      if File.exists?(path)
+        yaml = YAML.load_file(path)
+        @entry_date_xpath = yaml['xpath']['entry_date']
+        @entry_link_xpath = yaml['xpath']['entry_link']
+        @next_page_link_xpath = yaml['xpath']['next_page_link']
+      else
+        raise "Configuration file not found: #{path}"
+      end
     end
   end
 end
