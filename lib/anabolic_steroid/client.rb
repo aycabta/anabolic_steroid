6522245179632b3d9ded3333ffc65d7ab5011969
Code Ass <aycabta@gmail.com>
2016-10-11 04:31:16 +0900

    Split method chain

--- lib/anabolic_steroid/client.rb
+++ lib/anabolic_steroid/client.rb
@@ -36,11 +36,13 @@ module AnabolicSteroid
 
     def retrieve_date(page, block, *args)
       unless @config.entry_link_xpath.nil?
-        page.parser.xpath(@config.entry_link_xpath).each do |entry|
+        hit = page.parser.xpath(@config.entry_link_xpath)
+        hit.each do |entry|
           link = page.link_with(entry)
           absolute_link = page.uri.merge(link.uri).to_s
           @agent.get(absolute_link) do |entry_page|
-            block.call(to_date(entry_page.parser.xpath(@config.entry_date_xpath).first.to_s))
+            date_str = entry_page.parser.xpath(@config.entry_date_xpath).first.to_s
+            block.call(to_date(date_str))
           end
         end
       else
