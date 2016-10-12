7d316404a541e89c854a9ca1f3efffe485c9ec58
Code Ass <aycabta@gmail.com>
2016-10-12 23:46:14 +0900

    Receive some variables in Config

--- lib/anabolic_steroid/config.rb
+++ lib/anabolic_steroid/config.rb
@@ -1,18 +1,5 @@
-require 'yaml'
-
 module AnabolicSteroid
   class Config
-    attr_accessor :entry_date_xpath, :entry_link_xpath, :next_page_link_xpath
-
-    def initialize(path)
-      if File.exists?(path)
-        yaml = YAML.load_file(path)
-        @entry_date_xpath = yaml['xpath']['entry_date']
-        @entry_link_xpath = yaml['xpath']['entry_link']
-        @next_page_link_xpath = yaml['xpath']['next_page_link']
-      else
-        raise "Configuration file not found: #{path}"
-      end
-    end
+    attr_accessor :entry_link_xpath, :next_page_link_xpath
   end
 end
